f2eea062f25f5c1d879895c3132369736303e4aa
Niklas <146081495+NiklasEschelmuller@users.noreply.github.com>
2025-04-01 08:53:47 +0200

    Lösung des Problems das er keine eingabe anzeigt. Problem war das es das falsche Projekt Typ war.

---
+++ logic/AWinner.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorApp1;
+
+public abstract class AWinner
+{
+    protected abstract string? DiagonalWinner2();
+    protected abstract string? HorizontalWinner();
+    protected abstract string? VerticalWinner();
+    protected abstract string? DiagonalWinner1();
+
+    protected virtual string? CheckWinner()
+    {
+        string? w = HorizontalWinner();
+        if (w == null) w = VerticalWinner();
+        if (w == null) w = DiagonalWinner1();
+        if (w == null) w = DiagonalWinner2();
+        /* if (w == null)
+             return "No Winner / Draw";*/
+
+        return w;
+    }
+}
---
+++ logic/IGame.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp1;
+
+interface IGame
+{
+    public string? this [int row, int col] { get; }
+    public string? Winner { get; }
+    public string? NextPlayer { get; }
+    public void Set(int row, int col);
+}
+
+interface IGameV
+{
+    string? Winner { get; }
+    void PlacePiece(int column);
+    string[,] Field { get; }
+
+}
