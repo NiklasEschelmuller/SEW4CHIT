35840d53daf07a62a55d2a05cea45155ea0d288c
Niklas <146081495+NiklasEschelmuller@users.noreply.github.com>
2025-04-07 09:58:41 +0200

    Fünf Gewinnt Angefnangen rest wieder hergestellt

--- logic/FünfGeGame.cs
+++ logic/FünfGeGame.cs
@@ -1,6 +1,32 @@
-﻿namespace Game;
+﻿namespace BlazorApp1;
 
-public class FünfGeGame
+public class FünfGeGame: IGame
 {
+    private readonly string?[,] _field = new string[15, 15];
+
+    public string? Winner { get; private set; }
     
+    
+    public string NextPlayer { get; private set; } = "O";
+    public string? this[int x, int y] => _field[x, y] ?? " ";
+
+    public void Set(int x, int y)
+    {
+        if (_field[x, y] == null && Winner == null)
+        {
+            _field[x, y] = NextPlayer;
+            //Winner = CheckWinner();
+            
+            if (NextPlayer == "X")
+            {
+                NextPlayer = "O";
+            }
+            else
+            {
+                NextPlayer = "X";
+            }
+        }
+    }
+
+
 }
