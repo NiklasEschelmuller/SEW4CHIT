4b465ae7dbb84c3dc2621c169594f73f3c794583
Niklas <146081495+NiklasEschelmuller@users.noreply.github.com>
2025-04-08 08:36:25 +0200

    mehere minen

--- logic/Minesweeper.cs
+++ logic/Minesweeper.cs
@@ -14,10 +14,18 @@ public class Minesweeper
 
     private void MineSet()
     {
-        
-        int x = _rand.Next(0, 17);
-        int y = _rand.Next(0, 17);
-        _field[x, y] = "!!";
+        int amountm = _rand.Next(5, 20);
+        while (amountm > 0)
+        {
+            int x = _rand.Next(0, 17);
+            int y = _rand.Next(0, 17);
+            if (_field[x, y] == null)
+            {
+                _field[x, y] = "!!";
+                amountm--;
+            }
+            
+        }
     }
 
    public void Set(int x, int y)
