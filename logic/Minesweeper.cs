c30ffb04a1608567df843a0d4637c1c1186f83e2
Niklas <146081495+NiklasEschelmuller@users.noreply.github.com>
2025-04-22 08:19:38 +0200

    überarbeitung von minesweeper zu gewünschten form

--- logic/Minesweeper.cs
+++ logic/Minesweeper.cs
@@ -3,55 +3,41 @@ namespace BlazorApp1;
 
 public class Minesweeper
 {
-    private readonly string?[,] _field = new string[18, 18];
-    private readonly Random _rand = new();
-
-    public string? this[int x, int y] => _field[x, y] ?? " "; 
-    public Minesweeper()
+    private int[,] _field_;
+    private bool[,] opened;
+    
+    public Minesweeper(int columns, int rows)
     {
-        MineSet();
-
+        _field_ = new int[columns, rows];
+        opened = new bool[columns, rows];
     }
 
-    private void MineSet()
+    /*private void MineSet()
     {
         int amountm = _rand.Next(5, 20);
         while (amountm > 0)
         {
-            int x = _rand.Next(0, 17);
-            int y = _rand.Next(0, 17);
+            int x = _rand.Next(0, 16);
+            int y = _rand.Next(0, 16);
             if (_field[x, y] == null)
             {
                 _field[x, y] = "!!";
                 amountm--;
             }
+            
 
         }
-    }
+    }*/
 
-    private void CheckforMines(int x, int y)
-    {
-        int counter = 0;
-        for (int yi = y--; yi > y++; yi++)
-        {
-            for (int xi = x--; xi > x++; xi++)
-            {
-                if (_field[xi, yi] == "!!")
-                {
-                    counter++;
-                }
-            }
-        }
-         string c = counter.ToString();
-        _field[x, y] = c;
-        
-         
-    }
 
-   public void Set(int x, int y)
-    {
 
-    }
+   public string this [int col, int row] 
+   {
+       get
+       {
+           return _field_[col, row].ToString();
+       }
+   }
 }
 
 
