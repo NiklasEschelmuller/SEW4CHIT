309df0bb92806cd8c8aed0763aed19c759ed1fc0
Niklas <146081495+NiklasEschelmuller@users.noreply.github.com>
2025-09-15 15:23:02 +0200

    Ping Pong mit meherer Spielern beginnt noch bei spieler1

---
+++ Primzahl/Ringelern/Program.cs
@@ -0,0 +1,31 @@
+﻿const int ANZ = 6;
+//const int START = 3;
+SemaphoreSlim[] ringlern = new SemaphoreSlim[ANZ];
+
+for (int i = 0; i < ANZ; i++)
+{
+    if (i == 0) {
+        ringlern[i] = new SemaphoreSlim(1, 1);
+    }else {
+        ringlern[i] = new SemaphoreSlim(0, 1);
+    }
+}
+
+for (int i = 0; i < ANZ; i++)
+{
+    int id = i; 
+    new Thread(() => Spieler(id)).Start();
+}
+
+void Spieler(int id)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        ringlern[id].Wait();
+
+        Console.WriteLine($"Player{id + 1}");
+
+        int next = (id + 1) % ANZ;
+        ringlern[next].Release();
+    }
+}
