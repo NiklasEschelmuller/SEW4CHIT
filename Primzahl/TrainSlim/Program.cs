69d4e53f5185685d63bec954d0d602de6a13c9c4
Niklas <146081495+NiklasEschelmuller@users.noreply.github.com>
2025-09-25 12:02:04 +0200

    züge sind unterschiedlich lang

--- Primzahl/TrainSlim/Program.cs
+++ Primzahl/TrainSlim/Program.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+//Was noch zu erldigen ist.
 //Zug soll wenn er bei sationen ist blockieren so dass andere nicht da sind später soll er unterschiedlich lang sein
-//und die section sollen am anfang gespert sein so das man 1 drückt und der 1 wird frei
+//und die section sollen am anfang gespert sein so das man 1 drückt und der 1 wird frei etc. 
+//erste punkt soll keine stange sein. Output gibt an wenn zug station schließt oder wenn zug stion verlässt also sie wieder realseased
 string rail = new string('=', 70);
-List<int> trainpos = new List<int>();
+
+List<(int pos, int length)> trains = new List<(int pos, int length)>();
+
 SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
 bool running = true;
 
+
 int[] mastPositions = new int[10];
 for (int i = 0; i < mastPositions.Length; i++)
-    mastPositions[i] = i * 5;
+    mastPositions[i] = i * 10 + 10; 
 
 SemaphoreSlim[] mastSemaphores = new SemaphoreSlim[mastPositions.Length];
 for (int i = 0; i < mastSemaphores.Length; i++)
     mastSemaphores[i] = new SemaphoreSlim(1, 1);
 
 Console.Clear();
-
 
 void DrawMasts(char[] mastLine1, char[] mastLine2)
 {
@@ -32,8 +36,7 @@ Thread renderThread = new Thread(() =>
     while (running)
     {
         semaphore.Wait();
-
-
+        
         char[] mastLine1 = new char[rail.Length];
         char[] mastLine2 = new char[rail.Length];
         for (int i = 0; i < rail.Length; i++)
@@ -41,60 +44,72 @@ Thread renderThread = new Thread(() =>
             mastLine1[i] = ' ';
             mastLine2[i] = ' ';
         }
-        
+
         for (int i = 0; i < mastPositions.Length; i++)
         {
-            mastLine1[mastPositions[i]] = '\\';
-            mastLine2[mastPositions[i]] = '|';
+            if (mastPositions[i] < rail.Length)
+            {
+                mastLine1[mastPositions[i]] = '\\';
+                mastLine2[mastPositions[i]] = '|';
+            }
         }
         
         char[] zeile = rail.ToCharArray();
-        foreach (var pos in trainpos)
+
+        foreach (var train in trains)
         {
-            if (pos >= 0 && pos < rail.Length - 1)
+            int pos = train.pos;
+            int length = train.length;
+
+            for (int j = 0; j < length; j++)
             {
-                zeile[pos] = '|';
-                zeile[pos + 1] = '|';
-                
-                for (int m = 0; m < mastPositions.Length; m++)
-                {
-                    if (pos == mastPositions[m])
-                        mastLine1[mastPositions[m]] = '-';
-                }
+                if (pos + j >= 0 && pos + j < rail.Length)
+                    zeile[pos + j] = '|';
+            }
+
+
+            foreach (int mast in mastPositions)
+            {
+                if (pos <= mast && mast < pos + length)
+                    mastLine1[mast] = '-';
             }
         }
+
 
         DrawMasts(mastLine1, mastLine2);
         Console.SetCursorPosition(0, 3);
         Console.WriteLine(new string(zeile) + "   ");
 
 
-        for (int i = 0; i < trainpos.Count; i++)
+        for (int i = 0; i < trains.Count; i++)
         {
+            var (pos, length) = trains[i];
 
             bool blocked = false;
             for (int m = 0; m < mastPositions.Length; m++)
             {
-                if (trainpos[i] + 1 == mastPositions[m])
+                if (pos + length == mastPositions[m]) 
                 {
                     mastSemaphores[m].Wait();
                     blocked = true;
                 }
             }
 
-            trainpos[i]++;
-            
+            pos++;
+
             if (blocked)
             {
                 for (int m = 0; m < mastPositions.Length; m++)
                 {
-                    if (trainpos[i] > mastPositions[m] && mastSemaphores[m].CurrentCount == 0)
+                    if (pos > mastPositions[m] && mastSemaphores[m].CurrentCount == 0)
                         mastSemaphores[m].Release();
                 }
             }
+
+            trains[i] = (pos, length);
         }
         
-        trainpos.RemoveAll(p => p >= rail.Length);
+        trains.RemoveAll(t => t.pos >= rail.Length);
 
         semaphore.Release();
         Thread.Sleep(100);
@@ -103,6 +118,8 @@ Thread renderThread = new Thread(() =>
 renderThread.Start();
 
 ConsoleKey key;
+Random rand = new Random();
+
 do
 {
     key = Console.ReadKey(true).Key;
@@ -111,14 +128,19 @@ do
     {
         semaphore.Wait();
 
+
+        int length = rand.Next(2, 5);
+
         bool canStart = true;
         for (int m = 0; m < mastPositions.Length; m++)
         {
             if (mastPositions[m] == 0 && mastSemaphores[m].CurrentCount == 0)
                 canStart = false;
         }
+
         if (canStart)
-            trainpos.Add(0);
+            trains.Add((0, length));
+
         semaphore.Release();
     }
 
--- Primzahl/TrainSlim/obj/Debug/net9.0/TrainSlim.AssemblyInfo.cs
+++ Primzahl/TrainSlim/obj/Debug/net9.0/TrainSlim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainSlim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646706ced02aa2e41715bbad187cc0958ef837d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646544b499646967cbf9934e682be0b93d4c9d72")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainSlim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainSlim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
