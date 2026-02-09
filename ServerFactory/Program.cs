d82d39306d7e4c6720933a9cf7fff33000baabea
Niklas <146081495+NiklasEschelmuller@users.noreply.github.com>
2026-02-09 14:44:55 +0100

    verschidedene mime types jetzt verwendbar

--- ServerFactory/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-//
-
-//umschreiben das man factory method benutzt mit mime typ für fotos und pdf und primary conustrucator. 
-Console.WriteLine("Hello, World!");
-
-
